67eb257d55cd1b17b5148cf89ffdf324216b2339
Cesar Vallenilla <74629371+sibelius303@users.noreply.github.com>
2025-09-13 15:30:48 -0300

    correcciones de cors

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: '*' })); // temporal para pruebas
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
